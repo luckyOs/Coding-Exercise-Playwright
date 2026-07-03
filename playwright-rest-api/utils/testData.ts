6dc5874d7be36b71ccb7d24def93c45eb132505e
Oshani Lakchani <oshanilakchani@gmail.com>
2026-07-03 12:25:37 +0530

    Add test data payloads for Apple MacBook Pro

---
+++ playwright-rest-api/utils/testData.ts
@@ -0,0 +1,19 @@
+export const createPayload = {
+    name: "Apple MacBook Pro 16",
+    data: {
+        year: 2026,
+        price: 1999.99,
+        "CPU model": "Apple M4 Pro",
+        "Hard disk size": "1 TB"
+    }
+};
+
+export const updatePayload = {
+    name: "Apple MacBook Pro Updated",
+    data: {
+        year: 2026,
+        price: 2499,
+        "CPU model": "Apple M4 Max",
+        "Hard disk size": "2 TB"
+    }
+};
